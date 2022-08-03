7f29f907287b4242d646b8b5b5314232b214c0c5
ruslanzverkov <createmed@mail.ru>
2022-08-04 00:23:16 +0300

    DZ22

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,19 @@ public class Main {
         }
     }
 //2е задание
-    public static void installApp(int year, boolean os) {
-        if (year > 2015&&os) {
-            System.out.println("установите облегченную версию для "+os);
-        } else if (year <= 2015 && os) {
-            System.out.println("установите обычную версию для "+os);
+    public static void installApp(int year, int[] os) {
+        for (int i = 0; i < os.length; i++) {
+            if (os[i]==0&&year>2015) {
+                System.out.println("установите облегченную версию приложения для IOS");
+            } else if (os[i] == 0&&year<=2015) {
+                System.out.println("установите обычную версию приложения для IOS");
+            } else if (os[i] == 1 && year > 2015) {
+                System.out.println("установите облегченную версию приложения для Android");
+            } else {
+                System.out.println("установите обычную версию приложения для Android");
+            }
         }
+
     }
     public static void main(String[] args) {
 //       1е задание
@@ -22,10 +29,10 @@ public class Main {
         comeLeapYear(year);
 
 //        2е задание
-        boolean IOS=true;
-
-        boolean Android=true;
+        int[] oS={0,1};
+        int IO = oS[0];
+        int AN = oS[1];
         int yearOs=2016;
-        installApp(yearOs, IOS);
+        installApp(yearOs,oS);
     }
 }
