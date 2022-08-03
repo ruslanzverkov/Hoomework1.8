a2b48a9392383971db43a5a3cab81f5f70fdb09b
ruslanzverkov <createmed@mail.ru>
2022-08-03 21:23:03 +0300

    DZ2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+
+//   1е задание
+    public static void comeLeapYear(int leapYear) {
+        if (leapYear % 4 == 0) {
+            System.out.println(leapYear + "- високосный год");
+        } else {
+            System.out.println(leapYear+"-не високосный год");
+        }
+    }
+//
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//       1е задание
+        int year=2021;
+        comeLeapYear(year);
     }
 }
