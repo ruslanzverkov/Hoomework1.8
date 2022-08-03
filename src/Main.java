9eda24a622e7e8a2c4b469ea79f993a4905aefef
ruslanzverkov <createmed@mail.ru>
2022-08-03 22:12:02 +0300

    DZ2

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,24 @@ public class Main {
             System.out.println(leapYear+"-не високосный год");
         }
     }
-//
+//2е задание
+    public static void installApp(int year, boolean os) {
+        if (year > 2015&&os) {
+            System.out.println("установите облегченную версию для "+os);
+        } else if (year <= 2015 && os) {
+            System.out.println("установите обычную версию для "+os);
+        }
+    }
     public static void main(String[] args) {
 //       1е задание
         int year=2021;
         comeLeapYear(year);
+
+//        2е задание
+        boolean IOS=true;
+
+        boolean Android=true;
+        int yearOs=2016;
+        installApp(yearOs, IOS);
     }
 }
