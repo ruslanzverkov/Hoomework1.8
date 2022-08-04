0faaed0f3ea929575f2b6c9e5b63c4feb5ae6195
ruslanzverkov <createmed@mail.ru>
2022-08-04 18:31:49 +0300

    DZ3

--- src/Main.java
+++ src/Main.java
@@ -9,30 +9,32 @@ public class Main {
         }
     }
 //2е задание
-    public static void installApp(int year, int[] os) {
-        for (int i = 0; i < os.length; i++) {
-            if (os[i]==0&&year>2015) {
-                System.out.println("установите облегченную версию приложения для IOS");
-            } else if (os[i] == 0&&year<=2015) {
-                System.out.println("установите обычную версию приложения для IOS");
-            } else if (os[i] == 1 && year > 2015) {
-                System.out.println("установите облегченную версию приложения для Android");
-            } else {
-                System.out.println("установите обычную версию приложения для Android");
-            }
+    public static void installApp(int year, int os) {
+        if (year > 2015 && os == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (year > 2015 && os == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (year <= 2015 && os == 0) {
+            System.out.println("Установите обычную версию приложения для iOS по ссылке");
+        } else if (year <= 2015 && os == 1) {
+            System.out.println("Установите обычную версию приложения для Android по ссылке");
         }
-
     }
+
+
     public static void main(String[] args) {
+//
 //       1е задание
+
         int year=2021;
         comeLeapYear(year);
 
+//
 //        2е задание
-        int[] oS={0,1};
-        int IO = oS[0];
-        int AN = oS[1];
-        int yearOs=2016;
-        installApp(yearOs,oS);
+
+        int ios=0;
+        int androidOs=1;
+        int yearOs=2014;
+        installApp(yearOs,ios);
     }
 }
